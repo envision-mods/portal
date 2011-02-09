c1e8426ed9687529cf984b52433ddda78dd00ac0
live627 <live627@c1aa590f-bdde-4fb6-84b6-c067815d10c2>
2011-02-09 19:23:17 +0000

    + Added the CSS folder back in (ep_css, envisionportal.css) * Back to using jQuery UI for the draggable interface. This is miles better than the previous implementation. Oh yeah! (ManageEnvisionModules.php, ManageEnvisionModules.template.php, envisionportal.css)

--- trunk/ep_source/ManageEnvisionModules.php
+++ trunk/ep_source/ManageEnvisionModules.php
@@ -264,7 +264,8 @@ function ManageEnvisionModules()
 	$_SESSION['dlpIepos'] = $context['ep_columns']['disabled']['id_layout_position'];
 
 	$context['html_headers'] .= '
-	<script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js" type="text/javascript"></script>
 	<script type="text/javascript" src="' . $settings['default_theme_url'] . '/scripts/ep_scripts/ep_man_mods.js"></script>
 	<script type="text/javascript" src="' . $settings['default_theme_url'] . '/scripts/ep_scripts/ep_admin.js"></script>
 	<script type="text/javascript">
