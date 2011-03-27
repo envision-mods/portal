9c2caa6b77078d5d156b15cf38af4f5be8f49253
runic <runic@simplemachines.org>
2011-03-27 23:48:15 +0000

    + EnvisionPortal.english-utf8 file

---
+++ ep_languages/EnvisionPortal.english-utf8.php
@@ -0,0 +1,107 @@
+<?php
+// Version 1.0 EnvisionPortal
+
+// General Strings
+$txt['forum'] = 'Forum';
+$txt['ep_'] = 'Envision Portal';
+$txt['ep_core_modules'] = 'Collapse or expand this module';
+$txt['ep_who_forum'] = 'Viewing the forum index of <a href="' . $scripturl . '?action=forum">' . $context['forum_name'] . '</a>&#46;';
+$txt['ep_who_portal'] = 'Viewing the portal index of <a href="' . $scripturl . '">' . $context['forum_name'] . '</a>&#46;';
+$txt['ep_who_page'] = 'Viewing the page &quot;<a href="' ., $scripturl . '?page=%1$s">%2$s</a>&quot;&#46;';
+
+// Module-Specific Strings
+$txt['ep_module_usercp'] = 'User Panel';
+$txt['ep_inbox'] = 'Inbox';
+$txt['ep_outbox'] = 'Outbox';
+$txt['ep_time'] = 'Current Time';
+$txt['ep_login_user'] = 'Username';
+$txt['ep_user_posts'] = 'Posts';
+$txt['ep_user_replies'] = 'Replies';
+$txt['ep_module_stats'] = 'Statistics';
+$txt['ep_module_online'] = 'Who&#039;s Online';
+$txt['ep_module_announce'] = 'Announcement';
+$txt['ep_module_news'] = 'Site News';
+$txt['ep_module_posts'] = 'Recent Posts';
+$txt['ep_module_topics'] = 'Recent Topics';
+$txt['ep_module_search'] = 'Search';
+$txt['ep_match_all_words'] = 'Match all words';
+$txt['ep_match_any_words'] = 'Match any words';
+$txt['ep_module_poll'] = 'Poll';
+$txt['ep_module_calendar'] = 'Calendar';
+$txt['ep_holidays'] = 'Holidays';
+$txt['ep_bdays'] = 'Birthdays';
+$txt['ep_events'] = 'Events';
+$txt['ep_calendar_next'] = 'Next';
+$txt['ep_calendar_prev'] = 'Previous';
+$txt['ep_nocal_found'] = 'No calendar events were found&#46;';
+$txt['ep_module_topPosters'] = 'Top Posters';
+$txt['ep_module_staff'] = 'Forum Staff';
+$txt['ep_author'] = 'Author';
+$txt['ep_last_poster'] = 'Last Poster';
+$txt['ep_module_shoutbox'] = 'Shoutbox';
+$txt['ep_shoutbox_button'] = 'Shout';
+$txt['shoutbox_guest'] = 'Please login in order to use this shoutbox&#46;';
+$txt['shoutbox_view_msg'] = 'Viewing Shouts';
+$txt['shoutbox_no_msg'] = 'There are no messages to display at this time&#46;';
+$txt['shoutbox_message'] = 'Message';
+$txt['shoutbox_display_name'] = 'Display Name';
+$txt['shoutbox_poster_time'] = 'Poster Time';
+$txt['shoutbox_shout'] = 'Shout&#33;';
+$txt['shoutbox_emoticons'] = 'Emoticons';
+$txt['shoutbox_fonts'] = 'Fonts';
+$txt['shoutbox_history'] = 'History';
+$txt['ep_module_sitemenu'] = 'Site Navigation';
+$txt['ep_site_menu_alt_xall'] = 'Expand All';
+$txt['ep_site_menu_title_xall'] = 'Expand All Menus';
+$txt['ep_site_menu_alt_call'] = 'Collapse All';
+$txt['ep_site_menu_title_call'] = 'Collapse All Menus';
+$txt['ep_module_new_members'] = 'Latest Members';
+$txt['ep_module_theme_select'] = 'Theme Changer';
+$txt['ep_module_custom'] = 'Custom';
+$txt['ep_update'] = 'Update';
+$txt['ep_length'] = 'Length:';
+
+// Mod modules error handling&#46;  Use for all modules as you see fit&#33;
+$txt['ep_module_not_allowed'] = 'Access Denied&#33;';
+$txt['ep_module_mod_not_installed'] = 'Sorry, this mod hasn&#039;t been installed yet&#33;';
+$txt['ep_module_no_language'] = 'Unable to load the language file for this mod&#33;';
+$txt['ep_module_empty'] = 'No content to display for this module&#33;';
+$txt['ep_module_query_error'] = 'Error trying to obtain information for this mod&#46;<br />Mod must be reinstalled&#33;';
+
+// Admin Panel Strings
+$txt['ep_admin_config'] = 'Configuration';
+$txt['ep_admin_information'] = 'Information';
+$txt['ep_admin_general'] = 'General Settings';
+$txt['ep_admin_module_settings'] = 'Module Settings';
+$txt['ep_admin_modules'] = 'Modules';
+$txt['ep_admin_add_modules'] = 'Add Modules';
+$txt['ep_admin_manage_modules'] = 'Manage Modules';
+$txt['ep_admin_pages'] = 'Envision Pages';
+$txt['ep_admin_add_page'] = 'Add Page';
+$txt['ep_admin_manage_pages'] = 'Manage Pages';
+$txt['ep_admin_menu'] = 'Envision Menu';
+$txt['ep_admin_add_button'] = 'Add Button';
+$txt['ep_admin_manage_menu'] = 'Manage Menu';
+
+// Errors
+$txt['ep_module_error'] = 'There is an error with this module&#33;';
+$txt['epmod_uninstall_error'] = 'Unable to uninstall this module&#46;';
+$txt['ep_unable_to_view_file'] = 'Sorry, but either Envision Portal is disabled, or you don&#39;t have permission to view/download this file&#46;';
+
+// Envision Pages
+$txt['ep_pages_no_access_error'] = 'Error';
+$txt['ep_pages_no_access'] = 'You are not allowed to view this page&#46;';
+$txt['ep_pages_not_exist'] = 'That page does not exist&#33;';
+$txt['ep_pages_ajax_available'] = ' is <span style="color: #009933">available&#33;</span>';
+$txt['ep_pages_ajax_navailable'] = ' is <span style="color: #CC0000">not available&#33;</span>';
+
+// Module's file or function can't be found&#46;
+// %1$s = the name of the module, %2$s = the location of the module&#46;
+$txt['ep_modfile_not_exist'] = 'The module %1$s is missing it&#039;s file which should be located at: %2$s';
+$txt['ep_module_function_error'] = 'Unable to load up the function for this module&#46;';
+
+// Core Features in the AdminCP
+$txt['core_settings_item_ep'] = 'Envision Portal';
+$txt['core_settings_item_ep_desc'] = 'Envision Portal allows you to add the portal you have always envisiond to your forum&#46;';
+
+?>
