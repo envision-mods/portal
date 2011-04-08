14e9e53f2ddcf42bef9364544f55d7ee39c17610
live627 <live627@gmail.com>
2011-04-08 23:30:06 +0000

    + Added Unicode versions of files

---
+++ ep_languages/ManageEnvisionSettings.english-utf8.php
@@ -0,0 +1,83 @@
+<?php
+// Version 1.0; ManageEnvisionSettings
+
+// Main Title for this section.
+$txt['ep_admin_config'] = 'Envision Portal Configuration';
+
+// goes into the <title> tag of the document
+$txt['ep_admin_config_title'] = 'Envision Portal Configuration Settings';
+
+// Main description for this Envision Submenu/area/section.
+$txt['ep_admin_config_desc'] = 'This page allows you to change the settings of features and basic operations of Envision Portal.';
+
+// Just some overall help for this section, if we need it.
+$txt['ep_admin_config_help'] = 'Here you can view information about Envision Portal as well as change the basic features and options of Envision Portal.';
+
+// Descriptions here for each tab/page.
+$txt['ep_admin_config_info_desc'] = 'This page allows you to see announcements live from envisionportal.net as well as the people involved in making Envision Portal.';
+$txt['ep_admin_config_general_desc'] = 'This page allows you to change the features and options of Envision Portal.';
+
+/***********************************
+		Information Tab
+***********************************/
+// Title
+$txt['ep_admin_config_info'] = 'Envision Portal Info.';
+
+// Strings.
+$txt['ep_admin_config_latest_news'] = 'Live from Envision Portal...';
+$txt['ep_admin_config_unable_news'] = 'Unable to load the Envision Portal news file...';
+$txt['ep_admin_config_support_info'] = 'Support Information';
+$txt['ep_admin_config_version_info'] = 'Version Information';
+$txt['ep_admin_config_installed_version'] = 'Installed Version';
+$txt['ep_admin_config_latest_version'] = 'Latest Version';
+$txt['ep_outdated'] = 'You are using an outdated version of Envision Portal. Please update to the latest version as soon as possible.';
+
+// Credits strings.
+$txt['ep_credits'] = 'Credits';
+$txt['ep_credits_info'] = 'The Envision Portal Team would like to thank everyone who has made what Envision Portal is today. We also thank Simple Machines for making the software that this portal was built for as well as thanking YOU, the user, for using the portal.';
+$txt['ep_credits_groups_dev'] = 'Developers';
+$txt['ep_credits_groups_custom'] = 'Customization';
+$txt['ep_credits_groups_mar'] = 'Marketing';
+$txt['ep_credits_groups_docs'] = 'Documentation';
+$txt['ep_credits_groups_global'] = 'Globalization';
+$txt['ep_credits_special'] = 'Special Thanks';
+$txt['ep_credits_fam_fam'] = 'Icons';
+$txt['ep_credits_fam_fam_message'] = 'Mark James for his <a href="http://www.famfamfam.com/lab/icons/silk/" target="_blank">Fam Fam Fam Silk Icons</a>.';
+$txt['ep_credits_anyone'] = '<em>And for anyone we may have missed, thank you!</em>';
+$txt['ep_credits_contribute'] = 'Do you like Envision Portal? <a href="http://envisionportal.net/index.php?action=contribute">Contribute to the project!</a>';
+
+/***********************************
+		General Settings Tab
+***********************************/
+// Title
+$txt['ep_admin_config_general'] = 'General Settings';
+
+// Settings titles
+$txt['ep_collapse_modules'] = 'Enable collapsible modules';
+$txt['ep_color_members'] = 'Color members with member group color';
+
+/***********************************
+		Module Settings Tab
+***********************************/
+// Title
+$txt['ep_admin_config_modulesettings'] = 'Module Settings';
+
+// Description
+$txt['ep_admin_config_modulesettings_desc'] = 'Settings that apply to all your modules.';
+
+// Module settings
+$txt['ep_module_display_style'] = 'Module display style';
+$txt['ep_module_display_style_modular'] = 'Modular style';
+$txt['ep_module_display_style_blocks'] = 'Block style';
+$txt['ep_module_enable_animations'] = 'Enable module animations';
+$txt['ep_module_animation_speed'] = 'Module animation speed';
+$txt['ep_animation_speed_veryslow'] = 'Very slow';
+$txt['ep_animation_speed_slow'] = 'Slow';
+$txt['ep_animation_speed_normal'] = 'Normal';
+$txt['ep_animation_speed_fast'] = 'Fast';
+$txt['ep_animation_speed_veryfast'] = 'Very fast';
+$txt['ep_disable_custommod_icons'] = 'Do not install custom module icons';
+$txt['ep_enable_custommod_icons'] = 'Do not uninstall custom module icons';
+$txt['ep_icon_directory'] = 'Modules icon directory';
+
+?>
