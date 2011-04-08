f0b87bd66ce1ec2ad41d5fd8be5191b4c8300df1
live627 <live627@c1aa590f-bdde-4fb6-84b6-c067815d10c2>
2011-04-08 23:30:06 +0000

    + Added Unicode versions of files

---
+++ trunk/ep_languages/EnvisionPermissions.english-utf8.php
@@ -0,0 +1,11 @@
+<?php
+// Version 1.0; EnvisionPermissions
+
+// Place all permission strings for Envision Portal, including help strings, for permissions in here.
+
+$txt['permissiongroup_ep_'] = 'Envision Portal';
+$txt['permissiongroup_simple_ep_'] = 'Envision Portal';
+$txt['permissionname_ep_view'] = 'View Envision Portal';
+$txt['permissionhelp_ep_view'] = 'Allows the member to view Envision Portal';
+
+?>
