103b1594dca25af1b8d3180a18f24dc56320ee46
live627 <live627@gmail.com>
2011-05-26 05:58:53 +0000

    + Follow-up commit. Plugins are a serialized array within smf_settings. A dedicated table for them doesn't really seem right.

--- ep_source/Subs-EnvisionPlugins.php
+++ ep_source/Subs-EnvisionPlugins.php
@@ -106,7 +106,7 @@ function ep_include_hook($hook, $base = '')
 /**
  * {@see ep_call_hook}
  *
- * The file-inclusion hooks should be called with this.
+ * The language file-inclusion hooks should be called with this.
  *
  * The contents of the $modSettings['ep_hooks'] value is a comma separated list of files to be included at the relevant point.
  *
@@ -212,6 +212,139 @@ function ep_remove_hook($hook, $function)
 	}
 
 	return false;
+}
+
+/**
+ * Checks for an enabled plugin and runs an element of it.
+ *
+ * @param string $name The name of the plugin to call.
+ * @param string $function The name of the function within the plugin to call. Does not call any function if:
+ * - the function wasn't found
+ * - an empty string was passed
+ * @param array $parameters The parameters used when calling the function, if specified.
+ * @return mixed/bool False on failure; the return value of the called function; true if nno function called but the specifiedd plugin was found to be enabled.
+ */
+function ep_call_plugin($name, $function = '', $parameters = array())
+{
+	global $context, $modSettings;
+
+	if (!isset($modSettings['ep_plugins']))
+		return false;
+
+	$file = $context['ep_plugins_dir'] . '/' . $name . '/scripts/script.php';
+	$plugins = unserialize($modSettings['ep_plugins']);
+
+	if (!in_array($name, $plugins))
+		return false;
+
+	if (!file_exists($file))
+		return false;
+
+	require_once($file);
+
+	if (is_callable($function))
+		return call_user_func_array($function_name, $parameters);
+
+	return true;
+}
+
+/**
+ * Checks for an enabled plugin specified by $name and attempts to include its language specified by $lang. It defaults to the user's language if set and falls back to forum's global language.
+ *
+ * @param string $name The name of the plugin to call.
+ * @param array $lang a valid language string to use.
+ * $param int $try if first try will redo the function if language not found. It's highly recommended to leave this parameter alone.
+ * @return bool true if the specified language file was found and included; false otherwise.
+ */
+function ep_include_plugin_language($name, $lang = '', $try = 1)
+{
+	global $modSettings, $language, $user_info;
+
+	if (!isset($modSettings['ep_plugins']))
+		return false;
+
+	$base = $context['ep_plugins_dir'] . '/' . $name . '/languages';
+	$plugins = unserialize($modSettings['ep_plugins']);
+
+	if (!in_array($name, $plugins))
+		return false;
+
+	// Default to the user's language. Fall back to forum's global language
+	if ($lang == '')
+		$lang = isset($user_info['language']) ? $user_info['language'] : $language;
+
+	if (file_exists($base . '/' . $file . '/' . $lang . '.php'))
+		template_include($base . '/' . $file . '/' . $lang . '.php');
+	else
+		if ($try == 1)
+			ep_include_plugin_language($name, '', 2);
+		else
+			return false;
+
+	return true;
+}
+
+function ep_get_plugin_info($dirname, $file, $installing = false)
+{
+	global $boarddir, $context, $modSettings, $scripturl, $smcFunc;
+
+	// If the required info file does not exist let's silently die...
+	if (!file_exists($dirname . '/' . $file . '/plugin.xml'))
+		return false;
+
+	// And finally, get the file's contents
+	$file_info = file_get_contents($dirname . '/' . $file . '/plugin.xml');
+	$file_setting = $dirname . '/' . $file . '/scripts/script.php';
+	$func_name = 'plugin_' . $file . '_info';
+
+	// Parse info.xml into an xmlArray.
+	loadClassFile('Class-Package.php');
+	$plugin_info1 = new xmlArray($file_info);
+	$plugin_info1 = $plugin_info1->path('plugin[0]');
+
+	// Required XML elements and attributes. Quit if any one is missing.
+	if (!$plugin_info1->exists('title')) return false;
+	if (!$plugin_info1->exists('description')) return false;
+	if (!file_exists($file_setting)) return false;
+	require_once($file_setting);
+
+	// If installing, run the script.
+	if ($installing && is_callable($func_name))
+		$results = $func_name();
+	else
+		$results = array();
+
+	return array_merge(array(
+		'title' => $plugin_info1->fetch('title'),
+		'description' => ($plugin_info1->exists('description/@parsebbc')) ? ($plugin_info1->fetch('description/@parsebbc') ? parse_bbc($plugin_info1->fetch('description')) : $plugin_info1->fetch('description')) : $plugin_info1->fetch('description'),
+	), $results);
+}
+
+function listPlugins($installing = false)
+{
+	global $context, $modSettings, $sourcedir, $scripturl, $smcFunc, $txt;
+	$plugin_names = array();
+
+	// Let's loop throuugh each folder and get their plugin data. If anything goes wrong we shall skip it.
+	$files = scandir($context['ep_plugins_dir']);
+
+	foreach ($files as $file)
+	{
+		$retVal = ep_get_plugin_info($context['ep_plugins_dir'], $file, $installing);
+		if ($retVal === false)
+			continue;
+		else
+		{
+			$plugin_names[] = $file;
+			$plugin_info[$file] = $retVal;
+			$plugin_info[$file]['enabled'] = ep_call_plugin($file);
+		}
+	}
+
+	if (isset($plugin_info))
+		return $plugin_info;
+	else
+		return array();
 }
 
 ?>
