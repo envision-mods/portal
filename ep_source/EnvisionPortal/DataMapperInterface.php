b35df0014a8055fd726eb52a8a74b179d1e79fe3
John Rayes <live627@gmail.com>
2024-05-09 20:07:13 -0700

    Update DataMapperInterface.php

--- ep_source/EnvisionPortal/DataMapperInterface.php
+++ ep_source/EnvisionPortal/DataMapperInterface.php
@@ -1,25 +1,36 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * @package EnvisionPortal
+ * @version 2.0.2
+ * @author John Rayes <live627@gmail.com>
+ * @copyright Copyright (c) 2014, John Rayes
+ * @license MIT http://opensource.org/licenses/MIT
+ */
+
 namespace EnvisionPortal;
 
 interface DataMapperInterface
 {
 	/**
-	 * Generator that runs queries about attachment data and yields the result rows.
+	 * Fetches data from the database based on specified criteria.
 	 *
-	 * @param array    $selects Table columns to select.
-	 * @param array    $params  Parameters to substitute into query text.
-	 * @param array    $joins   or more *complete* JOIN clauses.
-	 *                          E.g.: 'LEFT JOIN {db_prefix}messages AS m ON (a.id_msg = m.id_msg)'
-	 * @param array    $where   Zero or more conditions for the WHERE clause.
-	 *                          Conditions will be placed in parentheses and concatenated with AND.
-	 *                          If this is left empty, no WHERE clause will be used.
-	 * @param array    $order   Zero or more conditions for the ORDER BY clause.
-	 *                          If this is left empty, no ORDER BY clause will be used.
-	 * @param array    $group
-	 * @param int|null $limit   Maximum number of results to retrieve.
-	 *                          If this is left empty, all results will be retrieved.
-	 * @param int|null $offset
+	 * @param array  $selects Table columns to select.
+	 * @param array  $params  Parameters to substitute into query text.
+	 * @param array  $joins   Zero or more *complete* JOIN clauses.
+	 *                        E.g.: 'LEFT JOIN messages AS m ON (a.id_msg = m.id_msg)'
+	 * @param array  $where   Zero or more conditions for the WHERE clause.
+	 *                        Conditions will be placed in parentheses and concatenated with AND.
+	 *                        If this is left empty, no WHERE clause will be used.
+	 * @param array  $order   Zero or more conditions for the ORDER BY clause.
+	 *                        If this is left empty, no ORDER BY clause will be used.
+	 * @param array  $group   Zero or more conditions for the GROUP BY clause.
+	 *                        If this is left empty, no GROUP BY clause will be used.
+	 * @param int    $limit   Maximum number of results to retrieve.
+	 *                        If this is left empty, all results will be retrieved.
+	 * @param int    $offset  Offset for LIMIT clause.
 	 *
 	 * @return array The result as associative array of database rows.
 	 */
@@ -34,13 +45,43 @@ interface DataMapperInterface
 		int $offset = null
 	): array;
 
+	/**
+	 * Inserts a new record into the database.
+	 *
+	 * @param EntityInterface $entity The entity to insert.
+	 */
 	public function insert(EntityInterface $entity): void;
 
+	/**
+	 * Updates an existing record in the database.
+	 *
+	 * @param EntityInterface $entity The entity to update.
+	 */
 	public function update(EntityInterface $entity): void;
 
+	/**
+	 * Deletes a record from the database.
+	 *
+	 * @param EntityInterface $entity The entity to delete.
+	 */
 	public function delete(EntityInterface $entity): void;
 
+	/**
+	 * Deletes multiple records from the database.
+	 *
+	 * @param array $ids Array of IDs of the records to delete.
+	 */
 	public function deleteMany(array $ids): void;
 
-	public function deleteAll(EntityInterface $entity): void;
+	/**
+	 * Deletes all records from the database.
+	 */
+	public function deleteAll(): void;
+
+	/**
+	 * Increments the views counter for a specific entity in the database.
+	 *
+	 * @param EntityInterface $entity The entity for which to increment the views counter.
+	 */
+	public function incrementViews(EntityInterface $entity): void;
 }
